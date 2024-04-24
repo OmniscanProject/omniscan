e38f88aa5000f84d38e18b3f8ebd24bc0c61ed74
Alexis Herold <alexis.herold13@gmail.com>
2024-04-24 11:53:11 +0200

    cleanup: homepage

--- resources/views/layouts/footer/copyright.blade.php
+++ resources/views/layouts/footer/copyright.blade.php
@@ -2,31 +2,25 @@
     <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav class="flex flex-wrap justify-center -mb-6 space-x-0 sm:space-x-12" aria-label="Footer">
             <div class="w-1/2 sm:w-auto pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">About</a>
+                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Acceuil</a>
             </div>
             <div class="w-1/2 sm:w-auto pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Blog</a>
+                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">À propos</a>
             </div>
             <div class="w-1/2 sm:w-auto pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Jobs</a>
+                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Services</a>
             </div>
             <div class="w-1/2 sm:w-auto pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Press</a>
+                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Contact</a>
             </div>
-            <div class="w-1/2 sm:w-auto pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Accessibility</a>
-            </div>
-            <div class="w-1/2 sm:w-auto pb-6">
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Partners</a>
+            <div>
+                @include('components.language-switcher')
             </div>
         </nav>
         <div class="flex flex-col justify-center items-center w-full space-y-4">
             <p class="text-center text-xs leading-5 text-gray-500">
                 &copy; 2024 Omiscan, tous droits réservés.
             </p>
-        
-            @include('components.language-switcher')
         </div>
-        
     </div>
 </footer>
